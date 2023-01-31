c34200e774a2ee6df62eacdd18f33a7c80b4d9dd
Jeronimo Kulandissa <jeronimo.kulandissa@opah.com.br>
2023-01-31 11:52:36 -0300

    Building Login component and setting Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
-      <h1> Let's build the linkedin clone !</h1>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Login />} />
+        </Routes>
+      </Router>      
     </div>
   );
 }
